c964c012b0d68305ff5cbcbeaab77c67011b7c4d
oscar <oscarvac74@gmail.com>
2023-05-15 19:00:16 -0500

    inital

--- backend/db/seeders/20230515233319-sports.js
+++ backend/db/seeders/20230515233319-sports.js
@@ -3,23 +3,36 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('Spots', [
+      {
+        ownerId: 1,
+        address: '123 Main Street',
+        city: 'San Francisco',
+        state: 'California',
+        country: 'United States',
+        lat: 37.7749,
+        lng: -122.4194,
+        name: 'Cozy Cabin',
+        description: 'A beautiful cabin in the woods',
+        price: 150.0,
+      },
+      {
+        ownerId: 2,
+        address: '456 Elm Street',
+        city: 'Los Angeles',
+        state: 'California',
+        country: 'United States',
+        lat: 34.0522,
+        lng: -118.2437,
+        name: 'Luxury Villa',
+        description: 'A luxurious villa with stunning views',
+        price: 500.0,
+      },
+    
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Spots', null, {});
   }
 };
