810f50bb4397bb03980f1c621646b383d3325e28
oscar <oscarvac74@gmail.com>
2023-05-16 00:11:16 -0500

    inital

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -4,31 +4,11 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Spot extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
-      Spot.belongsTo(models.User, {
-        foreignKey: 'ownerId',
-      }),
-      Spot.hasMany(models.Booking, {
-        foreignKey: 'spotId',
-        onDelete: 'CASCADE',
-        hooks: true
-      }),
-      Spot.hasMany(models.Review, {
-        foreignKey: 'spotId',
-        onDelete: 'CASCADE',
-        hooks: true
-      }),
-      Spot.hasMany(models.SpotImage, {
-        foreignKey: 'spotId',
-        onDelete: 'CASCADE',
-        hooks: true
-      })
+      Spot.belongsTo(models.User, {foreignKey: 'ownerId',}),
+      Spot.hasMany(models.Booking, {foreignKey: 'spotId',onDelete: 'CASCADE',}),
+      Spot.hasMany(models.Review, {foreignKey: 'spotId',onDelete: 'CASCADE',}),
+      Spot.hasMany(models.SpotImage, {foreignKey: 'spotId',onDelete: 'CASCADE',})
     }
   }
 
