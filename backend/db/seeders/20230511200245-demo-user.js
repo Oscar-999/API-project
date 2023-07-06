ba2115196accacc66ce9af3801f27f9ac90a0ac7
oscar <oscarvac74@gmail.com>
2023-07-06 14:59:55 -0500

    soft

--- backend/db/seeders/20230511200245-demo-user.js
+++ backend/db/seeders/20230511200245-demo-user.js
@@ -12,62 +12,27 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     options.tableName = 'Users';
     await queryInterface.bulkInsert(options, [
-      // {
-      //   firstName: 'Dylan',
-      //   lastName: 'Kingston',
-      //   email: 'kingdylan@gmail.com',
-      //   username: 'dylankinging',
-      //   hashedPassword: bcrypt.hashSync('password1')
-      // },
-      // {
-      //   firstName: 'Peter',
-      //   lastName: 'Cuh',
-      //   email: 'petoe@gmail.com',
-      //   username: 'PetierCuh',
-      //   hashedPassword: bcrypt.hashSync('password2')
-      // },
-      // {
-      //   firstName: 'Jose',
-      //   lastName: 'Jermenaz',
-      //   email: 'josej@gmail.com',
-      //   username: 'joseman',
-      //   hashedPassword: bcrypt.hashSync('password3')
-      // }
       {
-        firstName: 'Mordecai',
-        lastName: 'Yoo',
-        email: 'thepark@gmail.com',
-        username: 'RigbyMordecai',
+        firstName: 'Dylan',
+        lastName: 'Kingston',
+        email: 'kingdylan@gmail.com',
+        username: 'dylankinging',
         hashedPassword: bcrypt.hashSync('password1')
       },
       {
-        firstName: 'Tony',
-        lastName: 'Stark',
-        email: 'ironman@gmail.com',
-        username: 'Ironman',
+        firstName: 'Peter',
+        lastName: 'Cuh',
+        email: 'petoe@gmail.com',
+        username: 'PetierCuh',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
-        firstName: 'Gumball',
-        lastName: 'Watterson',
-        email: 'gumball@gmail.com',
-        username: 'Gumball',
+        firstName: 'Jose',
+        lastName: 'Jermenaz',
+        email: 'josej@gmail.com',
+        username: 'joseman',
         hashedPassword: bcrypt.hashSync('password3')
-      },
-      {
-        firstName: 'Son',
-        lastName: 'Goku',
-        email: 'demo@user.io',
-        username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password3')
-      },
-       {
-        firstName: 'Bruce',
-        lastName: 'Wayne',
-        email: 'bat@gmail.com',
-        username: 'Batman',
-        hashedPassword: bcrypt.hashSync('password3')
-      },
+      }
     ], {});
   },
   // 'RigbyMordecai', 'Ironman', 'Gumball', 'Demo-lition', 'Batman'
@@ -76,7 +41,7 @@ module.exports = {
     options.tableName = 'Users';
     const Op = Sequelize.Op;
     await queryInterface.bulkDelete(options, {
-      username: { [Op.in]: ['RigbyMordecai', 'Ironman', 'Gumball', 'Demo-lition', 'Batman'] }
+      username: { [Op.in]: ['dylankinging', 'PetierCuh', 'joseman'] }
     }, {});
   }
 };
