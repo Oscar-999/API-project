f2e2799d74666d7231237d3006d929b39fd9b007
oscar <oscarvac74@gmail.com>
2023-07-09 17:37:50 -0500

    test

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,94 +1,93 @@
 import React, { useState, useEffect, useRef } from "react";
-import { useDispatch } from 'react-redux';
-import * as sessionActions from '../../store/session';
-import OpenModalMenuItem from './OpenModalMenuItem';
-import LoginFormModal from '../LoginFormModal';
-import SignupFormModal from '../SignupFormModal';
+import { useDispatch } from "react-redux";
+import * as sessionActions from "../../store/session";
+import OpenModalMenuItem from "./OpenModalMenuItem";
+import LoginFormModal from "../LoginFormModal";
+import SignupFormModal from "../SignupFormModal";
 import { NavLink } from "react-router-dom";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 function ProfileButton({ user }) {
-    const dispatch = useDispatch();
-    const [showMenu, setShowMenu] = useState(false);
-    const ulRef = useRef();
-    const history = useHistory()
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef();
+  const history = useHistory();
 
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
 
-    const openMenu = () => {
-        if (showMenu) return;
-        setShowMenu(true);
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (!ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
     };
 
-    useEffect(() => {
-        if (!showMenu) return;
+    document.addEventListener("click", closeMenu);
 
-        const closeMenu = (e) => {
-            if (!ulRef.current.contains(e.target)) {
-                setShowMenu(false);
-            }
-        };
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
 
-        document.addEventListener('click', closeMenu);
+  const closeMenu = () => setShowMenu(false);
 
-        return () => document.removeEventListener("click", closeMenu);
-    }, [showMenu]);
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+    closeMenu();
+    history.push("/");
+  };
 
-    const closeMenu = () => setShowMenu(false);
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
 
-    const logout = (e) => {
-        e.preventDefault();
-        dispatch(sessionActions.logout());
-        closeMenu();
-        history.push('/')
-    };
+  return (
+    <>
+      <button onClick={openMenu}>
+        <i className="fas fa-user-circle" />
+      </button>
+      <ul className={ulClassName} ref={ulRef}>
+        {user ? (
+          <>
+            <li>
+              Hello, {user.firstName} {user.lastName}
+            </li>
+            <li>{user.username}</li>
+            <li>{user.email}</li>
+            <li>
+              <NavLink exact to="/spots/current">
+                Manage Spots
+              </NavLink>
+            </li>
+            <li>
+              <NavLink exact to="/reviews/current">
+                Manage Reviews
+              </NavLink>
+            </li>
 
-    const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
-
-    return (
-        <>
-            <button onClick={openMenu}>
-                 <i className="fas fa-user-circle" />
-            </button>
-            <ul className={ulClassName} ref={ulRef}>
-                {user ? (
-                    <>
-                        <li>Hello, {user.firstName} {user.lastName}</li>
-                        <li>{user.username}</li>
-                        <li>{user.email}</li>
-                        <li>
-                        <NavLink exact to='/spots/current'>
-                            Manage Spots
-                            </NavLink>
-                        </li>
-                        <li>
-                        <NavLink exact to='/reviews/current'>
-                            Manage Reviews
-                            </NavLink>
-                        </li>
-
-                        <li>
-                            <button onClick={logout}>
-                                Log Out
-                                </button>
-                        </li>
-                    </>
-                ) : (
-                    <>
-                        <OpenModalMenuItem
-                            buttonText="Log In"
-                            onItemClick={closeMenu}
-                            modalComponent={<LoginFormModal />}
-                        />
-                        <OpenModalMenuItem
-                            buttonText="Sign Up"
-                            onItemClick={closeMenu}
-                            modalComponent={<SignupFormModal />}
-                        />
-                    </>
-                )}
-            </ul>
-        </>
-    );
+            <li>
+              <button onClick={logout}>Log Out</button>
+            </li>
+          </>
+        ) : (
+          <>
+            <OpenModalMenuItem
+              buttonText="Log In"
+              onItemClick={closeMenu}
+              modalComponent={<LoginFormModal />}
+            />
+            <OpenModalMenuItem
+              buttonText="Sign Up"
+              onItemClick={closeMenu}
+              modalComponent={<SignupFormModal />}
+            />
+          </>
+        )}
+      </ul>
+    </>
+  );
 }
 
 export default ProfileButton;
