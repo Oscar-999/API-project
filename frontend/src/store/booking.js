e87e2ebcc6f4b546c3dc90ec23f532c2247370c9
oscar <oscarvac74@gmail.com>
2023-08-21 18:35:16 -0500

    booking

---
+++ frontend/src/store/booking.js
@@ -0,0 +1,12 @@
+import {csrfFetch} from './csrf'
+/************************************************************************************ */
+
+const CREATE_BOOKING = 'bookings/CREATE_BOOKING'
+const UPDATE_BOOKING = 'bookings/UPDATE_BOOKING'
+const GET_USER_BOOKING ='bookings/GET_USER_BOOKING'
+const DELETE_BOOKING = 'bookings/DELETE_BOOKING'
+
+
+const getUserBookings = (bookings) => {
+    
+}
